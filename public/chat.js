5eb6e31abfb9cf0ce704c752e266ca53c40a67e2
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-08-10 00:28:55 +0530

    Broadcast

--- public/chat.js
+++ public/chat.js
@@ -6,7 +6,7 @@ const message = document.querySelector("#message");
 const handle = document.querySelector("#handle");
 const btn = document.querySelector("#send");
 const output = document.querySelector("#output");
-
+const feedback = document.querySelector("#feedback");
 //Emit events
 btn.addEventListener("click", (e) => {
   socket.emit("chat", {
@@ -15,7 +15,15 @@ btn.addEventListener("click", (e) => {
   });
 });
 
+message.addEventListener("keypress", () => {
+  socket.emit("typing", { user: handle.value });
+});
+
 socket.on("chat", (data) => {
   console.log(data);
   output.innerHTML += `<p><strong>${data.handle}</strong> : ${data.message}`;
+});
+
+socket.on("typing", (data) => {
+  feedback.innerHTML = `${data.user} is typing`;
 });
