ef7ffe1669ce9740ec56e79a5bcd03200966f112
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-08-09 23:45:55 +0530

    Socket setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { urlencoded } = require("express");
+const socket = require("socket.io");
 const app = express();
 
 //middlewares
@@ -7,6 +8,14 @@ app.use(express.json());
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.listen(5000, () => {
+const server = app.listen(5000, () => {
   console.log("Listening to port 5000");
+});
+
+//Socket Setup
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log(socket);
+  console.log("Connection made");
 });
